3ecc2a83190ef786a960b7ed2736ae658bdd1553
Godstime Dike <103881889+Godstime171@users.noreply.github.com>
2022-07-13 12:42:13 +0000

    Solved challenges 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    sum += Number(arrayOfNumbers[i]);
+  }
+  return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  var count = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    if (arrayOfNumbers[i] % 2 === 0) {
+      count++;
+    }
+  }
+    return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -46,9 +56,14 @@ function countEvenNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
-function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
-}
+// let valueTemp = [];
+// function celsiusToFahrenheit(arrayOfNumbers){
+//   for (let i = 0; i < arrayOfNumbers.length; i++) {
+//     let fahrenValue = (arrayOfNumbers[i] * 1.8) + 32;
+//     valueTemp.push(Math.trunc(fahrenValue));
+//   }
+// }
+//     return valueTemp;
 // assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
